ecbf1300842cd12256a4e9c233c621b6d40da156
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-29 21:46:07 -0500

    add not found routes

---
+++ src/utilities/RoutesWithNotFound.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+function RoutesWithNotFound({ children }: Props) {
+  return (
+    <Routes>
+      {children}
+      <Route path={'*'} element={<h1>Not Found.</h1>} />
+    </Routes>
+  );
+}
+export default RoutesWithNotFound;

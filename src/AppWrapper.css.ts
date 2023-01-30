fd908b8f49df769c9b75ac8dab91fe81bd7a03df
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-30 15:57:14 -0500

    remove unused css

--- src/AppWrapper.css.ts
+++ src/AppWrapper.css.ts
@@ -1,26 +1,5 @@
 import styled from 'styled-components';
 
-const AppWrapper = styled.section`
-  display: flex;
-  justify-content: center;
-  width: 90%;
-  margin: 0 auto;
-  gap: 5rem;
-
-  .inputText,
-  .outputText {
-    width: 500px;
-  }
-
-  @media screen and (max-width: 750px) {
-    flex-direction: column;
-    align-items: center;
-
-    .inputText,
-    .outputText {
-      width: 90%;
-    }
-  }
-`;
+const AppWrapper = styled.section``;
 
 export default AppWrapper;

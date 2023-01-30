940914827e4eccb9c127d58dc1fcb7c2bf03a471
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-30 15:56:42 -0500

    add form and dispatch data

--- src/components/inputs/textInput.tsx
+++ src/components/inputs/textInput.tsx
@@ -1,15 +1,39 @@
 import TextField from '@mui/material/TextField';
+import { FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateText } from '../../redux/state/text';
+import { toBinary } from '../../utilities';
 
 function TextInput() {
+  const dispatch = useDispatch();
+
+  const [text, setText] = useState<string>('');
+
+  const handleForm = (e: FormEvent<HTMLInputElement | any>) => {
+    e.preventDefault();
+    setText(e.currentTarget.value);
+  };
+
+  const handleSaveText = (e: FormEvent, text: string) => {
+    e.preventDefault();
+    let bin = toBinary(text);
+    dispatch(updateText({ text: bin }));
+  };
+
   return (
-    <TextField
-      fullWidth
-      id='filled-multiline-static'
-      label='Write or paste your text.'
-      multiline
-      rows={12}
-      variant='filled'
-    />
+    <form onSubmit={(e) => handleSaveText(e, text)}>
+      <TextField
+        fullWidth
+        id='text'
+        label='Write or paste your text.'
+        multiline
+        rows={12}
+        variant='filled'
+        type='text'
+        onChange={handleForm}
+      />
+      <TextField color='primary' type='submit' />
+    </form>
   );
 }
 export default TextInput;

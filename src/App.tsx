ebaed99748cc132e188369507447bb93707275c0
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-29 21:49:50 -0500

    use footer and nav comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Route } from 'react-router-dom';
 import AppWrapper from './AppWrapper.css';
 import { lazy } from 'react';
 import { RoutesWithNotFound } from './utilities';
+import { Footer, Nav } from './components';
 
 const Home = lazy(() => import('./pages/home/home'));
 
 function App() {
   return (
     <AppWrapper>
+      <Nav />
       <RoutesWithNotFound>
         <Route path='/' element={<Home />} />
       </RoutesWithNotFound>
+      <Footer />
     </AppWrapper>
   );
 }

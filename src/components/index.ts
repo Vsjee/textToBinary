f07f4b7ea459ca51fed98adc411fed9632382a3b
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-30 10:10:08 -0500

    add mui app bar layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
+export * from './footer';
 export * from './inputs';
+export * from './nav';
 export * from './outputs';
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,20 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import HistoryIcon from '@mui/icons-material/History';
+
 function Nav() {
-  return <nav>Nav</nav>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            toBinary
+          </Typography>
+          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+            <HistoryIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 export default Nav;

8949f8a3fbe9a1689bb0cfe867894efdec15c4f5
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-29 21:46:34 -0500

    use not found utility component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import AppWrapper from './AppWrapper.css';
 import { lazy } from 'react';
+import { RoutesWithNotFound } from './utilities';
 
 const Home = lazy(() => import('./pages/home/home'));
 
 function App() {
   return (
     <AppWrapper>
-      <Routes>
+      <RoutesWithNotFound>
         <Route path='/' element={<Home />} />
-      </Routes>
+      </RoutesWithNotFound>
     </AppWrapper>
   );
 }

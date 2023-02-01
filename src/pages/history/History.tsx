a93099cb1ccadc92098f8e05887acc511c373d63
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-01 18:30:40 -0500

    add get data from local and display

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react';
+import { HistoryInfo } from '../../models';
+import { historyKey } from '../../redux/state/history';
+import { getLocalStorage } from '../../utilities';
 import HistoryWrapper from './HistoryWrapper.css';
 
 function History() {
-  return <HistoryWrapper>History</HistoryWrapper>;
+  const [history, setHistory] = useState<HistoryInfo[]>();
+
+  const saveHistory = () => {
+    const data: HistoryInfo[] = getLocalStorage(historyKey);
+    setHistory(data);
+  };
+
+  useEffect(() => {
+    saveHistory();
+  }, []);
+
+  return (
+    <HistoryWrapper>
+      <ul>
+        {history?.map((item, i) => (
+          <li key={i}>
+            {item.inputText}: {item.outputText} <button>delete</button>
+          </li>
+        ))}
+      </ul>
+    </HistoryWrapper>
+  );
 }
 export default History;

48b3c78e96d50e3b2168e647c70dc7ed20a9909f
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-01 18:30:11 -0500

    remove unused tsx

--- src/components/inputs/textInput.tsx
+++ src/components/inputs/textInput.tsx
@@ -1,15 +1,12 @@
 import TextField from '@mui/material/TextField';
 import { FormEvent, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { HistoryInfo } from '../../models';
-import { addHistoryItem, historyKey } from '../../redux/state/history';
+import { useDispatch } from 'react-redux';
+import { addHistoryItem } from '../../redux/state/history';
 import { updateText } from '../../redux/state/text';
-import { AppState } from '../../redux/store';
-import { setLocalStorage, toBinary } from '../../utilities';
+import { toBinary } from '../../utilities';
 
 function TextInput() {
   const dispatch = useDispatch();
-  const historyState = useSelector((state: AppState) => state.history.history);
 
   const [text, setText] = useState<string>('');
 
@@ -23,7 +20,6 @@ function TextInput() {
     let bin = toBinary(text);
     dispatch(updateText({ text: bin }));
     dispatch(addHistoryItem({ inputText: text, outputText: bin }));
-    setLocalStorage<HistoryInfo[]>(historyKey, historyState);
   };
 
   return (

64803045155f16bf4195f78404e8ffd4d97f976d
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-31 22:39:24 -0500

    use link for history route

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,5 +1,6 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import HistoryIcon from '@mui/icons-material/History';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
@@ -9,9 +10,11 @@ function Nav() {
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             toBinary
           </Typography>
-          <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-            <HistoryIcon />
-          </IconButton>
+          <Link to={'/history'} style={{ color: 'white' }}>
+            <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+              <HistoryIcon />
+            </IconButton>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>

67a0a734944d1c53b63e3e89b533ce59ce05fbe7
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-31 22:16:29 -0500

    add action to push in to history state

--- src/redux/state/history.ts
+++ src/redux/state/history.ts
@@ -1,10 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { HistoryState } from '../../models';
+import { HistoryInfo, HistoryState } from '../../models';
 
 const historyInitalState: HistoryState = { history: [] };
 
 export const historySlice = createSlice({
   name: 'history',
   initialState: historyInitalState,
-  reducers: {},
+  reducers: {
+    addHistoryItem: (state, actions) => {
+      let historyItem: HistoryInfo = {
+        inputText: actions.payload.inputText,
+        outputText: actions.payload.outputText,
+      };
+      state.history.push(historyItem);
+    },
+  },
 });
+
+export const { addHistoryItem } = historySlice.actions;

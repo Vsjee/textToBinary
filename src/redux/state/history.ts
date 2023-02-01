877e9b6081db62a7ef17f6fe006a8b69d878ad5d
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-31 22:38:30 -0500

    fix initial state for the history state

--- src/redux/state/history.ts
+++ src/redux/state/history.ts
@@ -3,9 +3,14 @@ import { HistoryInfo, HistoryState } from '../../models';
 
 const historyInitalState: HistoryState = { history: [] };
 
+export const historyKey = 'history';
+
 export const historySlice = createSlice({
   name: 'history',
-  initialState: historyInitalState,
+  initialState:
+    localStorage.getItem(historyKey) !== null
+      ? JSON.parse(localStorage.getItem(historyKey) as string)
+      : historyInitalState,
   reducers: {
     addHistoryItem: (state, actions) => {
       let historyItem: HistoryInfo = {

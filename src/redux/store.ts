16e0bc7ac6e68c772c9daa85a47168db20af2e17
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-01 19:07:32 -0500

    fix redux config for persist data

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { HistoryState, TextState } from '../models';
 import { historySlice } from './state/history';
 import { textSlice } from './state/text';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export interface AppState {
   text: TextState;
   history: HistoryState;
 }
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
+const rootReducer = combineReducers({
+  text: textSlice.reducer,
+  history: historySlice.reducer,
+});
+
+export const store = persistReducer(persistConfig, rootReducer);
+
 export const MY__STORE = configureStore({
-  reducer: {
-    text: textSlice.reducer,
-    history: historySlice.reducer,
-  },
+  reducer: store,
 });

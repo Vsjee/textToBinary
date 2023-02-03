52d86fa7ef4ad93dcd52e5aa73fe5241d54bcedb
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-02 21:33:49 -0500

    add a func to pass and modify the parent value

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -7,19 +7,25 @@ import HistoryTable from './components/HistoryTable';
 
 function History() {
   const [history, setHistory] = useState<HistoryInfo[] | any>();
+  const [bol, setBol] = useState(false);
 
   const saveHistory = () => {
     const data: HistoryInfo[] = getLocalStorage(historyKey);
     setHistory(data);
   };
 
+  const modifyHistory = (data: HistoryInfo[], rerender: boolean) => {
+    setBol(rerender);
+    setHistory(data);
+  };
+
   useEffect(() => {
     saveHistory();
-  }, []);
+  }, [bol]);
 
   return (
     <HistoryWrapper>
-      <HistoryTable history={history} />
+      <HistoryTable history={history} modifyHistory={modifyHistory} />
     </HistoryWrapper>
   );
 }

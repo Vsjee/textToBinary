63f96fbc1782d6fd1b721a728715595bd640f122
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-02 21:40:54 -0500

    style padding for the history page

--- src/pages/history/HistoryWrapper.css.ts
+++ src/pages/history/HistoryWrapper.css.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
-export const HistoryWrapper = styled.section``;
+export const HistoryWrapper = styled.section`
+  margin: 5rem 0 10rem 0;
+`;
 
 export default HistoryWrapper;

e944ba0f210cefbc20e101d56c8ff8104c1e9aab
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-03 22:18:41 -0500

    style footer

--- src/components/footer/FooterWrapper.css.ts
+++ src/components/footer/FooterWrapper.css.ts
@@ -2,6 +2,31 @@ import styled from 'styled-components';
 
 const FooterWrapper = styled.footer`
   margin-top: auto;
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.4rem;
+  border-top: 1px solid black;
+
+  .info {
+    font-size: 0.9rem;
+  }
+
+  .links {
+    display: inline-flex;
+    gap: 0.5rem;
+
+    a {
+      color: #000;
+      transition: 0.3s ease-in-out;
+
+      &:hover {
+        color: rgb(37, 150, 190);
+      }
+    }
+  }
 `;
 
 export default FooterWrapper;

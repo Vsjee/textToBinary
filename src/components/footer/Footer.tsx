6bc64edda17ca08ec2383c4567ba387ce54b6e06
david hernandez <davidfelipehernandez1@gmail.com>
2023-02-03 22:18:34 -0500

    add footer layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,20 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import FooterWrapper from './FooterWrapper.css';
 
 function Footer() {
-  return <FooterWrapper>Footer</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <div className='info'>Made by David Hernandez</div>
+      <div className='links'>
+        <a href='https://github.com/Vsjee' target='_blank'>
+          <GitHubIcon />
+        </a>
+        <a href='https://www.linkedin.com/in/david-felipe-hernandez-547109205/' target='_blank'>
+          <LinkedInIcon />
+        </a>
+      </div>
+    </FooterWrapper>
+  );
 }
 export default Footer;
